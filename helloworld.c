02aec50bba76b3a91ac68d971373efd149dcdec4
Frank Emanuel <emanuef@algonquincollege.com>
2025-05-05 12:17:45 -0400

    from first lecture

---
+++ helloworld.c
@@ -0,0 +1,28 @@
+/* filename.c
+ * 
+ * Prof. Frank Emanuel, Ph.D. 
+ * CST8234
+ * 2025-05-05
+ */
+
+#include <stdio.h>
+
+int main(){
+    
+
+    int i = 14;
+
+    printf("hello there world!\n");
+
+    printf("%d", i);
+
+/* ai generated code */
+
+    printf("\n");
+    printf("  :-)  \n");
+
+    return i;
+
+}
+
+
