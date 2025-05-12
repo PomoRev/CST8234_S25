0108829ac301b19962cc90ab5634ceb73b96c50d
Frank Emanuel <emanuef@algonquincollege.com>
2025-05-12 13:03:41 -0400

    comments for coding

--- shape.c
+++ shape.c
@@ -8,10 +8,20 @@
  * 2025-05-12
  */
 
+/* parse command line 
+    1) read it in 
+    2) ensure the commandline arguments are reasonable
+    3) set the size as a numeric value
+    4) create print function that take 
+       a character and number to print
+    5) print shape of specified size (triangle, 
+       square, diamond)     
+*/
+
 #include <stdio.h>
 #include <string.h>
 
-#define numberShapes 3
+
 
 int main(int argc, char *argv[]){
 
@@ -19,6 +29,7 @@ int main(int argc, char *argv[]){
     int i, ii;
     char shapeFound = 0;
     char shapes[3][10];
+
     strcpy(shapes[0], "triangle");
     strcpy(shapes[1], "square");
     strcpy(shapes[2], "diamond");
@@ -33,7 +44,7 @@ int main(int argc, char *argv[]){
             
             if (shapeFound == 0) error = 1;
 
-            
+
 
     }
 
