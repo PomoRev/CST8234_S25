99efc9ef748b5f2df1a8382c7eeda88f638a92c7
Frank Emanuel <emanuef@algonquincollege.com>
2025-07-20 19:35:04 -0400

    the days problem added

---
+++ btree.c
@@ -0,0 +1,23 @@
+/* btree.c   -> aw (analyze words)
+ * 
+ * Prof. Frank Emanuel, Ph.D. 
+ * CST8234
+ * 2025-07-20
+ */
+
+#include <stdio.h>
+
+int main(){
+
+/*     This program reads in a file of words using fscanf() and 
+    adds the words to a binary tree. When a duplicate word is found
+    a count in that word's structure will be incremented. 
+    The program will then print out the words in order (DFS) with 
+    the count of each individual word. */
+
+
+
+    return 0;
+}
+
+
